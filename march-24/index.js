9bdbf3b53b4e794668a5d46e36f5de01945efe55
sudhanshu kumar <sudhanshukumar8816@gail.com>
2022-03-24 22:05:49 +0530

    class code

---
+++ march-24/index.js
@@ -0,0 +1,117 @@
+
+// var a = [11,15,16,18,14,12,45,13,14]
+// // console.log(a.length)
+
+// // for(var i =0;i<array.length;i++)
+// //     console.log(array[i])
+
+
+// a.forEach(e =>{
+//    console.log(e)
+// })
+
+
+// function doJob(n){
+//    var n1 = 0
+//    for(var i =1 ; i<=n;i++){
+//        n1+=i
+//    }
+
+//     return n1 / n
+// }
+
+// console.log(doJob(6))
+
+
+// if you know number of iterations in advance
+
+// for(var i =0 ; i<=40;i++){
+
+// }
+
+// arr = [1,2]
+
+// arr.forEach(element => {
+    
+// });
+
+// for( ele of arr){
+
+// }
+
+
+// function returnRandomNumber(){
+//     return Math.floor(Math.random()*100)
+
+// }
+
+// while(returnRandomNumber() != 99){
+//     console.log("still running")
+// }
+
+
+
+// while(i<10){
+//     console.log(i)
+//     i=i+1
+// }
+// var i = 5
+// do{ 
+//     i+=1
+//     console.log(i)
+// }while(i<10)
+
+// function a(){
+//     console.log("this is a")
+// }
+
+
+// function b(){
+//     b()
+// }
+
+// console.log(b())
+
+
+// function calling itself is known as recursion
+
+
+
+// function doJob(){
+//     doJob()
+// }
+
+// doJob()
+
+// function printN(n){
+//     if(n<=0){
+//         return
+//     }
+//     printN(n-1)
+//     console.log(n)
+// }
+
+
+// printN(3)
+
+
+
+
+// function countNumberOfWays(n){
+//     if(n==1){
+//         return 1
+//     }
+//     if(n==2){
+//         return 2
+//     }
+//     if(n==3){
+//         return 3
+//     }
+//     var x1 = countNumberOfWays(n-1)
+//     var x2 = countNumberOfWays(n-2)
+//     var x3 = countNumberOfWays(n-3)
+
+//     return x1+x2+x3
+// }
+
+// console.log(countNumberOfWays(5))
